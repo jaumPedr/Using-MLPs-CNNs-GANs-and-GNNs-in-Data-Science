ab63ef1f8f798c20d5c082b8d4ef979c8e1d1559
jaumPedr <jpma306@gmail.com>
2026-06-07 10:06:21 -0300

    CNN update and code comments

--- src/CNN_Model.py
+++ src/CNN_Model.py
@@ -2,7 +2,7 @@ import torch
 from torch import nn
 from torch.utils.data import DataLoader, random_split
 from torchvision import datasets
-from torchvision.transforms import ToTensor
+from torchvision.transforms import ToTensor, transforms
 import torch.nn.functional as F
 from tqdm import tqdm
 from sklearn.metrics import confusion_matrix, accuracy_score
@@ -10,48 +10,69 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 import numpy as np
 
-#RANDOM SEED
-torch.manual_seed(42)
+RANDOM_SEED = 42
+BATCH_SIZE = 64
+TRAIN_SPLIT  = 0.8
+torch.manual_seed(RANDOM_SEED)
 
 def getData():
+
+    #Image normalization
+    tranform = transforms.Compose(
+            [
+                transforms.ToTensor(),
+                transforms.Normalize((0.5,), (0.5,)),  
+            ]
+        )
+
+    #Training dataset
     train_data = datasets.FashionMNIST(
         root="data",
         train=True,
         download=False,
-        transform=ToTensor()
+        transform=tranform
     )
-
-    test_data = datasets.FashionMNIST(
+    #Validation dataset
+    validation_data = datasets.FashionMNIST(
         root="data",
         train=False,
         download=False,
-        transform=ToTensor()
+        transform=tranform
     )
     
-    train_data_size = int( 0.8 * len(train_data.data) )
-    valid_data_size = len(train_data) - train_data_size
+    #Split training dataset into train and test subsets
+    train_data_size = int( TRAIN_SPLIT * len(train_data.data) )
+    test_data_size = len(train_data) - train_data_size
+    train_data, test_data = random_split( dataset = train_data, lengths= [train_data_size, test_data_size])
 
-    train_data, validation_data = random_split( dataset = train_data, lengths= [train_data_size, valid_data_size])
-
-
-    #image size 28x28x1, 10 classes (0 - 9)
+    #Create dataloaders
     train_dataloader = DataLoader(train_data, batch_size=64, shuffle=True)
     valid_dataloader = DataLoader(validation_data, batch_size=64, shuffle=True)
     test_dataloader = DataLoader(test_data, batch_size=64, shuffle=True)
 
     return train_dataloader, valid_dataloader, test_dataloader
 
+#Model:
+#Input Image -> Conv1 -> Pool1 -> Conv2 -> Pool2 -> Flatten -> Linear1 -> Linear2(Output)
 class Model(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1)
+        #Convolutional Layer 1
+        self.conv1 = nn.Conv2d(1, 64, kernel_size=3, stride=1, padding=1)
         self.pool1 = nn.MaxPool2d(kernel_size = 2, stride = 2, padding = 0)
 
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        #Convolutional Layer 2
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
         self.pool2 = nn.MaxPool2d(kernel_size = 2, stride = 2, padding = 0)
 
+        #Flatten 
         self.flatten = nn.Flatten()
-        self.linear1 = nn.Linear(64*7*7, 10)
+
+        #Fully Connected Hidden Layer 1
+        self.linear1 = nn.Linear(128*7*7, 128)
+
+        #Output Layer (10 classes)
+        self.linear2 = nn.Linear(128, 10)
 
 
     def forward(self, x):
@@ -63,13 +84,16 @@ class Model(nn.Module):
         x = self.pool2(x)
 
         x = self.flatten(x)
-        x = self.linear1(x)
+        x = F.relu(self.linear1(x))
+
+        x = self.linear2(x)
 
         return x
 
-def train_loop(dataloader, model, epochs = 40):
+def train_loop(dataloader, model, epochs = 20):
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
     model.train()
+
     for epoch in range(epochs):
         for batch, (X,y) in enumerate(tqdm(dataloader, desc = f"Epoch {epoch+1}/{epochs}")):
             pred = model(X)
@@ -96,21 +120,28 @@ def test_loop(dataloader, model):
             total += y.size(0)
     accuracy = correct / total if total > 0 else 0
     cm = confusion_matrix(y_true, y_pred)
-    last_X = X [0,0]
-    last_pred = y_pred [0]
-    return accuracy, cm, last_X, last_pred
+    return accuracy, cm
 
+
+#Load datasets
 train_dataloader, valid_dataloader, test_dataloader = getData()
+
+#Create model
 model = Model()
+
+#Train model
 train_loop(train_dataloader, model)
-accuracy, cm, last_X, last_pred = test_loop(test_dataloader, model)
+
+#Validation evaluation
+accuracy, cm= test_loop(valid_dataloader, model)
 print(f"Test Accuracy: {accuracy:.4f}")
 print("Confusion Matrix:")
 print(cm)
 
-#plt.figure(figsize=(10, 7))
-#plt.imshow(last_X, interpolation='nearest', cmap=plt.cm.Blues)
-#plt.title(last_pred)
-#plt.colorbar()
-#plt.show()
+#Final test evaluation
+accuracy, cm= test_loop(test_dataloader, model)
+print(f"Test Accuracy: {accuracy:.4f}")
+print("Confusion Matrix:")
+print(cm)
+
 
